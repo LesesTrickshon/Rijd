298811fa49f251c4d9a16094301bccf2902c299b
Leaf Tide <88660663+LesesTrickshon@users.noreply.github.com>
2025-05-07 12:53:33 +0200

    Add files via upload

---
+++ pages/setup/setup.js
@@ -0,0 +1,19 @@
+function save(){
+    let car_type = document.getElementById("car-option").value;
+    let verbrauch = document.getElementById("verbrauch").value;
+
+    console.log(car_type);
+    console.log(verbrauch);
+
+    localStorage.setItem("car_type", car_type);
+    localStorage.setItem("verbrauch", verbrauch);
+
+    if (localStorage.getItem("distance") === null || localStorage.getItem("distance") === 0){
+        localStorage.setItem("distance", 0);
+        localStorage.setItem("time", 0);
+        localStorage.setItem("emmision", 0);
+        console.log("No User Data found. Created new Variables");
+    }
+
+    window.location.href = "/pages/main/main.html"
+}
