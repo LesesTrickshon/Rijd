2be797571c8d5321dc5d845f4ba84387db83d1af
Leaf Tide <88660663+LesesTrickshon@users.noreply.github.com>
2025-05-07 12:52:50 +0200

    Add files via upload

---
+++ pages/settings/settings.js
@@ -0,0 +1,30 @@
+document.getElementById("verbrauch").value = localStorage.getItem("verbrauch");
+
+function save() {
+  let car_type = document.getElementById("car-option").value;
+  let verbrauch = document.getElementById("verbrauch").value;
+  console.log(car_type);
+  console.log(verbrauch);
+  localStorage.setItem("car_type", car_type);
+  localStorage.setItem("verbrauch", verbrauch);
+}
+
+function RM_Data() {
+  if (confirm("You are forcefully removing all of your Data!")) {
+    localStorage.removeItem("distance");
+    localStorage.removeItem("time");
+    localStorage.removeItem("emmision");
+    localStorage.removeItem("car-option");
+    localStorage.removeItem("verbrauch");
+
+    window.location.href = "/pages/setup/setup.html";
+  }
+}
+
+function Reset_Data() {
+  if (confirm("You are removing all your booked Data!")) {
+    localStorage.setItem("distance", 0);
+    localStorage.setItem("time", 0);
+    localStorage.setItem("emmision", 0);
+  }
+}
