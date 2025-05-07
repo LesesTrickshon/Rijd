59aba933623a57dc2f1c058b376ab520a700b457
Leaf Tide <88660663+LesesTrickshon@users.noreply.github.com>
2025-05-07 12:51:21 +0200

    Add files via upload

---
+++ pages/main/main.js
@@ -0,0 +1,3 @@
+document.getElementById("distance").innerHTML = 'Total distance: ' + localStorage.getItem("distance").toString() + 'km';
+document.getElementById("time").innerHTML = 'Time spent on road: ' + localStorage.getItem("time").toString() + 'min';
+document.getElementById("emmision").innerHTML = 'Carbon emmisions: ' + localStorage.getItem("emmision").toString() + 'kg';
